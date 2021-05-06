59e6b5cdfb36b647ed50266c7688cf39c01103e6
VitaliSantalau <v.santalau@gmail.com>
2021-05-06 17:52:34 +0300

    start stopwatch

---
+++ stopwatch/script.js
@@ -0,0 +1,12 @@
+const hour = document.querySelector('.hour');
+const minute = document.querySelector('.minute');
+const sec = document.querySelector('.sec');
+const startButton = document.querySelector('.start');
+const stopButton = document.querySelector('.stop');
+const resetButton = document.querySelector('.reset');
+
+let currentValue = 0;
+
+startButton.onClick = () => {
+  
+}
