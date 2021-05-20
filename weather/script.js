d146a73cc280efcd06b3e2d08117bd6eaa59bd2a
VitaliSantalau <v.santalau@gmail.com>
2021-05-20 17:48:15 +0300

    weather app - create current location

--- weather/script.js
+++ weather/script.js
@@ -1,22 +1,73 @@
-const example = document.querySelector('.example');
-const search = document.querySelector('.search-form');
-const inputCity = document.querySelector('.inputCity');
 const apiKey = "909202fe44f41f51f5e8e51cdf753b6e";
-const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`;
 
-async function getData() {
+const searchForm = document.querySelector('.searchForm');
+const inputCity = document.querySelector('.inputCity');
+const curentLocationButton = document.querySelector('.curentLocationButton');
+const outputCity = document.querySelector('.outputCity');
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const month = document.querySelector('.month');
+const day = document.querySelector('.day');
+
+let currentStateCity = {
+  default: "City didn't set",
+  location: {
+    latitude: '',
+    longitude: '',
+  },
+  inputNameCity: false,
+}
+
+function convertNumberMonthtoString(num) {
+  const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  return month[num];
+}
+
+async function outputDate() {
+  currentMoment = new Date();
+  let getHours = await currentMoment.getHours();
+  let getMins = await currentMoment.getMinutes();
+  let getMonth = await currentMoment.getMonth();
+  let getDay = await currentMoment.getDate();
+  hours.innerHTML = getHours < 10 ? `0${getHours}` : getHours;
+  minutes.innerHTML = getMins < 10 ? `0${getMins}` : getMins;
+  month.innerHTML = convertNumberMonthtoString(getMonth);
+  day.innerHTML = getDay < 10 ? `0${getDay}` : getDay;
+}
+
+async function getWeatherData() {
+
+
+
+
+  /*
+  const urlDataByCity = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const urlDataByLocation = `https://api.openweathermap.org/data/2.5/weather?lat=${currentStateCity.location.latitude}&lon=${currentStateCity.location.longitude}&appid=${apiKey}`
+  let url = inputCity.value ? urlDataByCity : urlDataByLocation;
   try {
     let response = await fetch(url);
     let data = await response.json();
     return data;
   } catch (error) {
     
-  }
+  }*/
 }
 
-search.addEventListener('submit', async e => {
+curentLocationButton.onclick = () => {
+  async function success(pos) {
+    let crd = pos.coords;
+    currentStateCity.location.latitude = crd.latitude;
+    currentStateCity.location.longitude = crd.longitude;
+    const { name } = await getWeatherData();
+     outputCity.innerHTML = `<div>${name}</div>`;
+  }
+  navigator.geolocation.getCurrentPosition(success);
+}
+
+searchForm.addEventListener('submit', async e => {
   e.preventDefault();
-  const searchingCity = inputCity.value;
-  const { main } = await getData();
-  example.innerHTML = `<div>${main.temp}</div>`;
+  const { name } = await getWeatherData(inputCity.value);
+  outputCity.innerHTML = `<div>${name}</div>`;
   })
+
+document.addEventListener("DOMContentLoaded", outputDate())
